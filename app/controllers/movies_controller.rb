30a6d83009cef77852d912fe44fa9467a0d8bd60
Siddharth Sharma <siddharth@maropost.com>
2017-12-17 19:49:46 +0000

    callbacks before action for index page added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   before_action :set_id, except:[:new,:create,:index]
+  before_action :get_toprated, only:[:index]
+  before_action :get_topviewed, only:[:index]
 
   def show
     @movies_list = Movie.all.order('rating desc')
@@ -7,8 +9,8 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all.first(3)
-    @movies_toprated = Movie.all.order('rating desc').limit(6)
-    @movies_topviewed = Movie.all.order('rating asc').limit(6)
+    @movies_toprated = @movies_toprated.limit(6)
+    @movies_topviewed = @movies_topviewed.limit(6)
   end
   def edit
 
@@ -31,8 +33,17 @@ class MoviesController < ApplicationController
   def allowed_params
     params.require(:movie).permit(:title, :genre, :plot, :rating, :web, :image, :year, :cast)
   end
+
   def set_id
     @movies = Movie.find(params[:id])
+  end
+
+  def get_toprated
+    @movies_toprated = Movie.all.order('rating desc')
+  end
+
+  def get_topviewed
+    @movies_topviewed = Movie.all.order('rating asc')
   end
 
 end
