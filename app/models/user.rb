b48b10b693e2d138d25364247284ef53aa7dc1f9
Siddharth Sharma <siddharth@maropost.com>
2017-12-20 03:43:22 +0000

    image and name field added in users and also in comment submit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  mount_uploader :image, UserUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :comments
   has_many :movies, through: :comments
+
+  validates_presence_of :name
 end
